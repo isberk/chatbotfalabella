efb8c29cfd78b38e6f479bbd397c0044d4b5d1b2
isberk <pokewei059@gmail.com>
2025-12-10 19:25:30 -0500

    Update start.sh ss

--- rasa_project_saga/start.sh
+++ rasa_project_saga/start.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-rasa run --enable-api --cors "*" --interface 0.0.0.0 --port $PORT
+set -e
+
+echo "PORT=$PORT"
+
+rasa run \
+  --enable-api \
+  --cors "*" \
+  --interface 0.0.0.0 \
+  --port $PORT
